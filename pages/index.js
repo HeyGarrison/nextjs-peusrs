83199335a47c1e11be1bec6e05b9f6deec5df69d
HeyGarrison <garrison@speedbumpit.com>
2022-02-05 21:13:38 -0600

    blah blah

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next</title>
       </Head>
 
       <main className={styles.main}>
